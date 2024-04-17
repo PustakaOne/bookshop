00a9f50713c4f9e9a643d079ae3b0e5665da6528
steven-fo <stevenfo0711@gmail.com>
2024-04-17 18:47:41 +0700

    [GREEN] Implement Book Controller

--- src/main/java/id/ac/ui/cs/pustakaone/bookshop/controller/BookController.java
+++ src/main/java/id/ac/ui/cs/pustakaone/bookshop/controller/BookController.java
@@ -1,2 +1,18 @@
-package id.ac.ui.cs.pustakaone.bookshop.controller;public class BookController {
+package id.ac.ui.cs.pustakaone.bookshop.controller;
+
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;;
+
+@RestController
+@RequestMapping("/")
+public class BookController {
+    @GetMapping("books")
+    public String getBooks() {
+        return "Book List";
+    }
+
+    @GetMapping("book/{id}")
+    public String getBookDetail(@PathVariable("id") int id, Model model) {
+        return "Book 1";
+    }
 }
