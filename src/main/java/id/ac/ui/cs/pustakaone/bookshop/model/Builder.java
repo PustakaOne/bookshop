55964119efe97398657ee20d8d229650c71b1caa
steven-fo <stevenfo0711@gmail.com>
2024-04-24 20:00:12 +0700

    feat: create builder for book

--- src/main/java/id/ac/ui/cs/pustakaone/bookshop/model/Builder.java
+++ src/main/java/id/ac/ui/cs/pustakaone/bookshop/model/Builder.java
@@ -1,4 +1,21 @@
 package id.ac.ui.cs.pustakaone.bookshop.model;
 
-public interface Builder {
+import java.util.Date;
+import java.util.List;
+
+public interface BookBuilder {
+    public void reset();
+    public void setBookId(String id);
+    public void setTitle(String title);
+    public void setAuthor(String author);
+    public void setPublisher(String publisher);
+    public void setDescription(String description);
+    public void setPrice(int price);
+    public void setStock(int stock);
+    public void setReleaseDate(Date releaseDate);
+    public void setIsbn(String isbn);
+    public void setCoverUrl(String coverUrl);
+    public void setCategory(String category);
+    public void setPages(int pages);
+    String void setLang(String lang);
 }
