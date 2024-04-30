b09d8049298a08b096fc7dbd9dc74dbfdaec95a2
steven-fo <stevenfo0711@gmail.com>
2024-04-30 19:13:29 +0700

    feat: implement jpa to model review

--- src/main/java/id/ac/ui/cs/pustakaone/bookshop/model/Review.java
+++ src/main/java/id/ac/ui/cs/pustakaone/bookshop/model/Review.java
@@ -1,18 +1,39 @@
 package id.ac.ui.cs.pustakaone.bookshop.model;
 
-import lombok.Getter;
-import lombok.Setter;
+import jakarta.persistence.*;
+import lombok.*;
 
 import java.util.Date;
 
 @Getter
 @Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "Review")
 public class Review {
+    @Id
+    @Column(name = "id")
     private String id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @Column(name = "bookId")
     private Book book;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @Column(name = "userId")
     private User user;
+
+    @Column(name = "content")
     private String review;
+
+    @Column(name = "rating")
     private int rating;
+
+    @Column(name = "createdAt")
     private Date createdAt;
+
+    @Column(name = "updatedAt")
     private Date updatedAt;
 }
