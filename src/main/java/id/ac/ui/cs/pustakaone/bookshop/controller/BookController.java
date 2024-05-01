cc682f4e6f2c45084b354e4c7450763d738a0a73
steven-fo <stevenfo0711@gmail.com>
2024-05-01 20:52:43 +0700

    [GREEN] Implement Book controller

--- src/main/java/id/ac/ui/cs/pustakaone/bookshop/controller/BookController.java
+++ src/main/java/id/ac/ui/cs/pustakaone/bookshop/controller/BookController.java
@@ -1,18 +1,31 @@
 package id.ac.ui.cs.pustakaone.bookshop.controller;
 
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;;
+import id.ac.ui.cs.pustakaone.bookshop.model.Book;
+import id.ac.ui.cs.pustakaone.bookshop.service.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
 
 @RestController
-@RequestMapping("/")
 public class BookController {
-    @GetMapping("books")
-    public String getBooks() {
-        return "Book List";
+    @Autowired
+    private BookService bookService;
+
+    @RequestMapping(value = "/books", method = RequestMethod.GET)
+    public ResponseEntity<List<Book>> getAllBooks() {
+        ResponseEntity<List<Book>> responseEntity = null;
+        List<Book> books = bookService.getAllBooks();
+        responseEntity = ResponseEntity.ok(books);
+        return responseEntity;
     }
 
-    @GetMapping("book/{id}")
-    public String getBookDetail(@PathVariable("id") int id, Model model) {
-        return "Book 1";
+    @RequestMapping(value = "/book/{id}", method = RequestMethod.GET)
+    public ResponseEntity<Book> getBookDetail(@PathVariable long id) {
+        ResponseEntity<Book> responseEntity = null;
+        bookService.getBookDetail(id);
+        responseEntity = ResponseEntity.ok().build();
+        return responseEntity;
     }
 }
