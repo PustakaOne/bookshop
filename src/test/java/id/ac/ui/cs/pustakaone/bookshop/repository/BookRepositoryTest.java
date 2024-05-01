6badf53bccd3bfa7954630b0f48ab859f7d19763
steven-fo <stevenfo0711@gmail.com>
2024-05-01 16:06:53 +0700

    chores: remove unused import

--- src/test/java/id/ac/ui/cs/pustakaone/bookshop/repository/BookRepositoryTest.java
+++ src/test/java/id/ac/ui/cs/pustakaone/bookshop/repository/BookRepositoryTest.java
@@ -7,7 +7,6 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 
 import id.ac.ui.cs.pustakaone.bookshop.model.Book;
 import java.util.List;
