39084955eb92877bf1769a2953143c15c424435e
steven-fo <stevenfo0711@gmail.com>
2024-05-01 20:52:27 +0700

    [RED] Add testing for book controller

--- src/test/java/id/ac/ui/cs/pustakaone/bookshop/controller/BookControllerTest.java
+++ src/test/java/id/ac/ui/cs/pustakaone/bookshop/controller/BookControllerTest.java
@@ -1,12 +1,21 @@
 package id.ac.ui.cs.pustakaone.bookshop.controller;
 
+import id.ac.ui.cs.pustakaone.bookshop.model.Book;
+import id.ac.ui.cs.pustakaone.bookshop.service.BookService;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import java.util.Collections;
+import java.util.List;
 
 import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
@@ -14,23 +23,46 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @ExtendWith(MockitoExtension.class)
 public class BookControllerTest {
+    @Mock
+    private BookService bookService;
+
     @InjectMocks
     private BookController bookController;
+
     private MockMvc mockMvc;
+    private Book book;
+
+    @BeforeEach
+    void setUp() {
+        mockMvc = MockMvcBuilders.standaloneSetup(bookController).build();
+        book = Book.builder()
+                .title("Tes")
+                .author("tes")
+                .publisher("Gramed")
+                .stock(20)
+                .price(200000)
+                .build();
+    }
 
     @Test
     void testGetBookList() throws Exception {
-        mockMvc = MockMvcBuilders.standaloneSetup(bookController).build();
+        List<Book> bookList = Collections.singletonList(book);
+        ResponseEntity<List<Book>> expectedResponse = ResponseEntity.ok(bookList);
+        when(bookService.getAllBooks()).thenReturn(expectedResponse);
 
-        mockMvc.perform(get("/books/"))
-                .andExpect(status().isOk()).andExpect(content().string("Book List"));
+        mockMvc.perform(get("/books"))
+                .andExpect(status().isOk())
+                .andExpect(content().json("[{\"title\":\"Tes\",\"author\":\"tes\",\"publisher\":\"Gramed\",\"stock\":20,\"price\":200000}]"));
     }
 
     @Test
     void testGetBookDetail() throws Exception {
-        mockMvc = MockMvcBuilders.standaloneSetup(bookController).build();
+        bookService.addNewBook(book);
+        Book savedBook = bookService.getBookDetail(book.getBookId());
+        when(bookService.getBookDetail(book.getBookId())).thenReturn(savedBook);
 
         mockMvc.perform(get("/book/1"))
-                .andExpect(status().isOk()).andExpect(content().string("Book 1"));
+                .andExpect(status().isOk())
+                .andExpect(content().json("[{\"title\":\"Tes\",\"author\":\"tes\",\"publisher\":\"Gramed\",\"stock\":20,\"price\":200000}]"));
     }
 }
