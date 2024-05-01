9185f40bc27966c16a25ab75b6b90af7878302a9
edbert2397 <edberthalim29@gmail.com>
2024-05-01 07:43:48 +0700

    [green] edit bookcart model

--- src/main/java/id/ac/ui/cs/pustakaone/bookshop/model/Cart.java
+++ src/main/java/id/ac/ui/cs/pustakaone/bookshop/model/Cart.java
@@ -1,47 +1,54 @@
 package id.ac.ui.cs.pustakaone.bookshop.model;
 
+import jakarta.persistence.*;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
+@NoArgsConstructor
+@Getter
+@Setter
+@Table(name = "Cart")
+@Entity
 public class Cart {
+    public Cart(String userId) {
+        this.userId = userId;
+        this.totalPrice = 0;
+        this.paymentSuccess = false;
+        this.bookCarts = new ArrayList<>();
+        this.address = "";
+    }
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private String id;
-    @Setter
+
+    @Column(name = "userId")
+    private String userId;
+
+    @Column(name = "totalPrice")
     private int totalPrice;
-    private PaymentState paymentState;
-    @Getter
+
+    @Column(name = "paymentSuccess")
+    private boolean paymentSuccess;
+
+    @OneToMany(mappedBy = "cart")
     private List<BookCart> bookCarts;
 
-    public Cart(String id) {
-        this.id = id;
-        this.bookCarts = new ArrayList<>();
-        this.paymentState = new NotPaidState(); // default state
-    }
+    @Column(name = "address")
+    private String address;
 
-    public int getTotalHarga() {
-        return totalPrice;
-    }
+    @Column(name = "paidAt")
+    private Date paidAt;
 
-    public String getPaymentStatus() {
-        return paymentState.getStatus();
-    }
-
-    public void processPayment() {
-        this.paymentState.processPayment(this);
-    }
-
-    public void completePayment() {
-        this.paymentState.completePayment(this);
-    }
-
-    public void addBookCart(BookCart bookCart) {
-        this.bookCarts.add(bookCart);
-    }
-
-    public void setState(PaymentState state) {
-        this.paymentState = state;
+    public void setTotalPrice(int totalPrice) {
+        if (totalPrice < 0) {
+            throw new IllegalArgumentException("Total price tidak boleh negatif");
+        }
+        this.totalPrice = totalPrice;
     }
 }
