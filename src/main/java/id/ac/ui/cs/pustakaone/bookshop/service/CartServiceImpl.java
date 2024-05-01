f54d6d3b26915e058678728a0d4e156796bff72a
daffarafip24 <daffarafip24@gmail.com>
2024-05-01 16:31:01 +0700

    [GREEN] Implement getCartByUserId service

--- src/main/java/id/ac/ui/cs/pustakaone/bookshop/service/CartServiceImpl.java
+++ src/main/java/id/ac/ui/cs/pustakaone/bookshop/service/CartServiceImpl.java
@@ -1,14 +1,23 @@
 package id.ac.ui.cs.pustakaone.bookshop.service;
 
 import id.ac.ui.cs.pustakaone.bookshop.model.Cart;
+import id.ac.ui.cs.pustakaone.bookshop.repository.CartRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class CartServiceImpl implements CartService {
+    @Autowired
+    CartRepository cartRepository;
     @Override
     public Cart getCartByUserId(String userId) {
-        return null;
+        Cart cart = cartRepository.findByUserIdAndPaymentSuccessIsFalse(userId);
+        if (cart == null) {
+            cart = new Cart(userId);
+            cartRepository.save(cart);
+        }
+        return cart;
     }
 }
