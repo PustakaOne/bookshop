e45ead975563886f5c0261d3a40214da6e18e6ae
steven-fo <stevenfo0711@gmail.com>
2024-05-02 14:38:58 +0700

    fix: remove unused import

--- src/main/java/id/ac/ui/cs/pustakaone/bookshop/service/BookServiceImpl.java
+++ src/main/java/id/ac/ui/cs/pustakaone/bookshop/service/BookServiceImpl.java
@@ -5,7 +5,6 @@ import id.ac.ui.cs.pustakaone.bookshop.repository.BookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.Date;
 import java.util.List;
 
 @Service
