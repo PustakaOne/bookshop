e334f73f9cecb6e8550edacd7ee3539a442e9b2d
steven-fo <stevenfo0711@gmail.com>
2024-05-02 14:43:04 +0700

    merge from feat/review

--- src/main/java/id/ac/ui/cs/pustakaone/bookshop/controller/BookController.java
+++ src/main/java/id/ac/ui/cs/pustakaone/bookshop/controller/BookController.java
@@ -1,18 +1,40 @@
 package id.ac.ui.cs.pustakaone.bookshop.controller;
 
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;;
+import id.ac.ui.cs.pustakaone.bookshop.model.Book;
+import id.ac.ui.cs.pustakaone.bookshop.service.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
 
 @RestController
-@RequestMapping("/")
 public class BookController {
-    @GetMapping("books")
-    public String getBooks() {
-        return "Book List";
+    @Autowired
+    private BookService bookService;
+
+    @RequestMapping(value = "/books", method = RequestMethod.GET)
+    public ResponseEntity getAllBooks() {
+        ResponseEntity responseEntity = null;
+        try {
+            List<Book> books = bookService.getAllBooks();
+            responseEntity = ResponseEntity.ok(books);
+        } catch (Exception e) {
+            responseEntity = ResponseEntity.badRequest().body(HttpStatus.BAD_REQUEST);
+        }
+        return responseEntity;
     }
 
-    @GetMapping("book/{id}")
-    public String getBookDetail(@PathVariable("id") int id, Model model) {
-        return "Book 1";
+    @RequestMapping(value = "/book/{id}", method = RequestMethod.GET)
+    public ResponseEntity getBookDetail(@PathVariable long id) {
+        ResponseEntity responseEntity = null;
+        try {
+            bookService.getBookDetail(id);
+            responseEntity = ResponseEntity.ok().body(bookService.getBookDetail(id));
+        } catch (Exception e) {
+            responseEntity = ResponseEntity.badRequest().body(HttpStatus.BAD_REQUEST);
+        }
+        return responseEntity;
+
     }
 }
