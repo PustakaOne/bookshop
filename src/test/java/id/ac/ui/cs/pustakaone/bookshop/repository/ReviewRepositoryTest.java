1deba55c55b068ffcb3597cb26e4c2f034b480ca
steven-fo <stevenfo0711@gmail.com>
2024-05-25 20:54:33 +0700

    merge from dev

--- src/test/java/id/ac/ui/cs/pustakaone/bookshop/repository/ReviewRepositoryTest.java
+++ src/test/java/id/ac/ui/cs/pustakaone/bookshop/repository/ReviewRepositoryTest.java
@@ -4,7 +4,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import id.ac.ui.cs.pustakaone.bookshop.model.Review;
-import id.ac.ui.cs.pustakaone.bookshop.model.User;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,7 +20,6 @@ public class ReviewRepositoryTest {
 
     private Book book;
     private Review review;
-    private User user;
 
     @BeforeEach
     public void setUp() {
@@ -30,10 +28,8 @@ public class ReviewRepositoryTest {
                 .author("Test")
                 .price(200000)
                 .build();
-        user = new User("Tes", "tes", "0899", "pass123");
         review = Review.builder()
                 .bookId(book.getBookId())
-                .userId(user.getId())
                 .content("Sebuah review")
                 .rating(5)
                 .build();
@@ -43,11 +39,9 @@ public class ReviewRepositoryTest {
     void testCreateReview() {
         Review createdReview = reviewRepository.save(review);
         assertNotNull(createdReview);
-        assertEquals(createdReview.getUserId(), user.getId());
         assertEquals(createdReview.getContent(), review.getContent());
         assertEquals(createdReview.getRating(), review.getRating());
         assertEquals(createdReview.getBookId(), book.getBookId());
-        assertEquals(createdReview.getUserId(), user.getId());
     }
 
     @Test
@@ -56,7 +50,6 @@ public class ReviewRepositoryTest {
         Optional<Review> savedReview = reviewRepository.findById(review.getReviewId());
         assertNotNull(savedReview);
         assertEquals(savedReview.get().getBookId(), book.getBookId());
-        assertEquals(savedReview.get().getUserId(), user.getId());
         assertEquals(savedReview.get().getContent(), review.getContent());
         assertEquals(savedReview.get().getRating(), review.getRating());
     }
@@ -65,7 +58,6 @@ public class ReviewRepositoryTest {
     void testGetAllBookReview() {
         Review review2 = Review.builder()
                 .bookId(book.getBookId())
-                .userId(user.getId())
                 .content("Sebuah review")
                 .rating(5)
                 .build();
@@ -93,7 +85,6 @@ public class ReviewRepositoryTest {
         Review updatedReview = Review.builder()
                 .reviewId(review.getReviewId())
                 .bookId(book.getBookId())
-                .userId(user.getId())
                 .content("Sebuah review updated")
                 .rating(4)
                 .build();
